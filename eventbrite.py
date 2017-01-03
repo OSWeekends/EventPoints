dbd9eedb024f0ded47ca7af7824b0da1a2ff73bb
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-01-03 09:29:50 +0000

    Esqueleto de la petición

--- eventbrite.py
+++ eventbrite.py
@@ -1,1 +1,23 @@
-# https://www.eventbrite.com/d/spain--madrid/science-and-tech--events/madrid/?crt=regular&sort=date
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import urllib
+
+url = "https://www.eventbrite.com/d/spain--madrid/science-and-tech--events/madrid/?crt=regular&sort=date"
+request = urllib.urlopen(url)
+
+print "status code: " + str(request.getcode())
+if request.getcode() == 200:
+    request = request.read()
+    soup = BeautifulSoup(request)
+    
+    events = soup.findAll("div", { "class" : "search-date-group"})
+
+    if events:
+        for event in events:
+            print event
+    else: 
+        print "No existen eventos!"
+    
+    # print request
+else:
+    print "Error con la petición."
