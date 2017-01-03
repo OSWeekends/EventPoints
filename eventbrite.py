d9634eb698093c972fa5094f33c1d5ec9d9a7be2
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-01-03 10:23:56 +0000

    Migración a python3

--- eventbrite.py
+++ eventbrite.py
@@ -1,23 +1,27 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
-import urllib
+from urllib.request import urlopen
 
 url = "https://www.eventbrite.com/d/spain--madrid/science-and-tech--events/madrid/?crt=regular&sort=date"
-request = urllib.urlopen(url)
+request = urlopen(url)
 
-print "status code: " + str(request.getcode())
 if request.getcode() == 200:
     request = request.read()
-    soup = BeautifulSoup(request)
+    soup = BeautifulSoup(request, "html5lib")
     
     events = soup.findAll("div", { "class" : "search-date-group"})
 
     if events:
         for event in events:
-            print event
+            eventSoup = BeautifulSoup(event.decode_contents(formatter="html"), "html5lib")
+            fecha = eventSoup.find("time", {"class": "list-card__date"})
+            titulo = eventSoup.find("div", {"class": "list-card__title"})
+            if titulo and fecha:
+                print(titulo.text.strip())
+                print(fecha.text.strip())
     else: 
-        print "No existen eventos!"
+        print("No existen eventos!")
     
     # print request
 else:
-    print "Error con la petición."
+    print("Error con la petición.")
