3ad74778c15e5174a73413f771f532f934e8e921
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-01-04 17:16:23 +0000

    refactoring

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var exec = require('child_process').exec,
-    fs = require('fs'),
-    project = require('pillars'),
-    firebase = require('firebase'),
-    config = require('./config'),
-    harmonizer = require("./harmonizer.js");
+  fs = require('fs'),
+  project = require('pillars'),
+  firebase = require('firebase'),
+  config = require('./config'),
+  harmonizer = require("./harmonizer.js");
 
 // Bring data from datasources
 var data = harmonizer();
@@ -15,7 +15,7 @@ firebase.initializeApp({
     clientEmail: config.firebase.client_email,
     privateKey: config.firebase.private_key
   },
-  databaseURL: "https://"+config.firebase.project_id+".firebaseio.com"
+  databaseURL: "https://" + config.firebase.project_id + ".firebaseio.com"
 });
 
 var db = firebase.database();
@@ -23,33 +23,35 @@ var ref = db.ref("events");
 
 // Starting the project
 project.services.get('http').configure({
-    port: process.env.PORT || 3000
+  port: process.env.PORT || 3000
 }).start();
 
 // Define Rutes
 var apiRoute = new Route({
-    id: 'staticRoute',
-    path: '/api',
-    cors: true
-    }, function(gw){
-        gw.redirect("/");
+  id: 'staticRoute',
+  path: '/api',
+  cors: true
+}, function(gw) {
+  gw.redirect("/");
 });
 
 var apiEventsRoute = new Route({
-    id: 'staticRoute',
-    path: 'api/events',
-    cors: true
-    }, function(gw){
-        gw.json(data, {deep:10});
+  id: 'staticRoute',
+  path: 'api/events',
+  cors: true
+}, function(gw) {
+  gw.json(data, {
+    deep: 10
+  });
 });
 
 var staticRoute = new Route({
-    id: 'staticRoute',
-    path: '/*:path',
-    directory: {
-        path: './public',
-        listing: true
-    }
+  id: 'staticRoute',
+  path: '/*:path',
+  directory: {
+    path: './public',
+    listing: true
+  }
 });
 
 // Adding routes objects to the project
@@ -57,53 +59,50 @@ project.routes.add(apiEventsRoute);
 project.routes.add(apiRoute);
 project.routes.add(staticRoute);
 
-
-
-
 function readData() {
-    ref.once("value", function(snapshot) {
-        data = snapshot.val();
-    }, function (errorObject) {
-        console.log("Error:", errorObject);
-    });
+  ref.once("value", function(snapshot) {
+    data = snapshot.val();
+  }, function(errorObject) {
+    console.log("Error:", errorObject);
+  });
 }
 
-function updateData(){
-    ref.set(data, function(error) {
-      if (error) {
-        console.log("Data could not be saved." + error);
-      } else {
-        console.log("Data saved successfully.");
+function updateData() {
+  ref.set(data, function(error) {
+    if (error) {
+      console.log("Data could not be saved." + error);
+    } else {
+      console.log("Data saved successfully.");
+    }
+  });
+}
+
+function pythonRocks() {
+  fs.readdir('./datasource/', (err, files) => {
+    files.forEach(file => {
+      if (/.py/.test(file)) {
+        console.log(`---- Proceso hijo de ${file} Iniciado! ------`);
+        exec('cd datasource && python3 ' + file, function(error, stdout, stderr) {
+          console.log(`---- Proceso hijo de ${file} terminado! -----`);
+          if (stdout) {
+            console.log('stdout: ' + stdout);
+          }
+
+          if (stderr) {
+            console.log('stderr: ' + stderr);
+          }
+
+          if (error) {
+            console.log('exec error: ' + error);
+          }
+        });
       }
     });
+  });
 }
 
-function pythonRocks () {
-    fs.readdir('./datasource/', (err, files) => {
-      files.forEach(file => {
-          if(/.py/.test(file)){
-            console.log(`---- Proceso hijo de ${file} Iniciado! ------`);
-            exec('cd datasource && python3 '+ file, function(error, stdout, stderr) {
-                console.log(`---- Proceso hijo de ${file} terminado! -----`);
-
-                if(stdout){
-                    console.log('stdout: ' + stdout);
-                }
-
-                if(stderr){
-                    console.log('stderr: ' + stderr);
-                }
-
-                if (error) {
-                    console.log('exec error: ' + error);
-                }
-            });
-          }
-      });
-    });
-};
-
-//updateData();
-//readData();
+// pythonRocks()
+// updateData();
+// readData();
 
 // Cron Tasks
