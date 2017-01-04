83244a6bf6c6b445d27323819c122fdc22382039
L.Carquet <laetiticarquet@gmail.com>
2017-01-04 18:52:09 +0100

    scrap location url and details

--- datasource/media_lab_prado.py
+++ datasource/media_lab_prado.py
@@ -2,45 +2,85 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 import urllib.request
-import datetime
+import datetime as dt
 
-date = "2017-01-06"
+# now = dt.datetime.now()
+# date = dt.datetime.strptime(str(now.year) +" " + date.text.strip(), '%Y %a, %b %d\n %H:%M %p')
+# date.isoformat()
+today = (time.strftime("%y-%m-%d"))
+now = dt.datetime.now()
+
+date = today
 
 url = "http://medialab-prado.es/events/" + date
 request = urllib.request.urlopen(url)
+
+eventsList = []
 
 if request.getcode() == 200:
 	request = request.read()
 	soup = BeautifulSoup(request, "html.parser")
 
-	pageevents = soup.find("ul", { "class" : "lista"})
+	pageevents = soup.find("ul", { "class" : "lista"}).findChildren("li")
 
 	if pageevents:
-		pageevents = pageevents.findChildren("a")
 
 		for event in pageevents:
-				if event.text == "Seguir leyendo…":
-					event_url = event['href']
-					request2 = urllib.request.urlopen(event_url)
+			currentEvent = {
+                "location": {"name": "MediaLab Prado","lon": 40.411321,"lat": -3.693456},
+                "price": {},
+                "source": {
+                    "name": "MediaLab Prado",
+                    "logo": "http://blogs.medialab-prado.es/streamlab/wp-content/uploads/sites/28/2016/01/00_logo-medialab-madrid.jpg",
+                    "url": "http://medialab-prado.es/"
+                }
+            }
 
-					if request2.getcode() == 200:
-						request2 = request2.read()
-						soup = BeautifulSoup(request2, "html.parser")
+		eventSoup = BeautifulSoup(event.decode_contents(formatter="html"), "html.parser")
+		# 	# date
 
-						location = soup.find("div", { "class" : "lugar"})
-						if location == None:
-							location = "MediaLab"
-						else:
-							location = location.find("p")
-						print (location)
+		# 	# time
 
-						description = soup.find("div", { "class" : "entradilla"})
-						print(description.text)
+		# 	notes
+		location = eventSoup.find("div",{"class": "lugar"})
+		if location == None:
+			location = "MediaLab"
+		else:
+			location = location.find("p").text.strip()
+			currentEvent["location"]["notes"] = location
 
+		# 	# name of the event
+		name = eventSoup.find("a")
+		currentEvent["title"] = name.text
+
+		# 	# abstract
+		abstract = eventSoup.find("blockquote",{"class": "entradilla"})
+		currentEvent["abstract"] = abstract.text
+
+		#url
+		event_url = eventSoup.find("a", href=True)
+		currentEvent["target_url"] = event_url['href']
+		currentEvent["source"]["event_url"] = event_url['href']
+
+
+		# 	####### EVENT_URL- TARGET URL
+		# 	# event = event.find("a")
+		# 	if event.text == "Seguir leyendo…":
+		# 		event_url = event['href']
+		# 		request2 = urllib.request.urlopen(event_url)   #url of the event
+
+
+		#
+
+		# 		if request2.getcode() == 200:
+		# 			request2 = request2.read()
+		# 			soup = BeautifulSoup(request2, "html.parser")
+
+		#
 
 	else:
 		print("no existen eventos")
 else:
-    print("Error con la petición.")
+	print("Error con la petición.")
 
 
