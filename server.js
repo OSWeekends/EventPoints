a81269804f2ba39b03ea8b63e7c3f6a32ad99976
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-01-04 09:58:12 +0000

    Estaticos + HTTP Server

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Server :-)
+var project = require('pillars'),
+    config = require('./config');
+
+// Starting the project
+project.services.get('http').configure({
+    port: process.env.PORT || 3000
+}).start();
+
+// Static
+var staticRoute = new Route({
+    id: 'staticRoute',
+    path: '/*:path',
+    directory: {
+        path: './public',
+        listing: true
+    }
+});
+
+// Adding routes objects to the project
+project.routes.add(staticRoute);
