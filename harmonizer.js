590f6fa7c7226ddf09ac8ec9cdc631c80f64cfa2
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-01-04 15:42:27 +0000

    Estructura modular y mejoras

--- harmonizer.js
+++ harmonizer.js
@@ -2,35 +2,38 @@ var _ = require("lodash"),
     uuidV4 = require('uuid/v4'),
     fs = require('fs'),
     evenbrite = require("./datasource/output/eventbrite.json"),
-    campusMadrid = require("./datasource/output/campus_madrid.json");
+    campusMadrid = require("./datasource/output/campus_madrid.json"),
+    firebase = require('firebase');
 
-var arrayEventos = [];
+module.exports = function(){
+    var arrayEventos = [];
 
-// Merging Arrays
-arrayEventos = campusMadrid.concat(evenbrite);
+    // Merging Arrays
+    arrayEventos = campusMadrid.concat(evenbrite);
 
-// Clean the data
-evenbrite = campusMadrid = null;
+    // Clean the data
+    evenbrite = campusMadrid = null;
 
 
+    /*
+        Filtering duplicate elements
+        @see https://lodash.com/docs/4.17.4#uniqBy
+    */
+    arrayEventos = _.uniqBy(arrayEventos, "target_url");
+
+    // adding UUIDs
+    arrayEventos.forEach(function(event){
+        event.id = uuidV4()
+    })
 /*
-    Filtering duplicate elements
-    @see https://lodash.com/docs/4.17.4#uniqBy
+    fs.writeFile('./final.json', JSON.stringify(arrayEventos , null, 4), function (err) {
+        if (!err) {
+            console.log('.output/final.json -- Actualizado!');
+        } else {
+            console.log('ERROR FATAL!! Al guardar ./final.json');
+            throw err;
+        }
+    });
 */
-arrayEventos = _.uniqBy(arrayEventos, "target_url");
-
-// adding UUIDs
-arrayEventos.forEach(function(event){
-    event.id = uuidV4()
-})
-
-fs.writeFile('./final.json', JSON.stringify(arrayEventos , null, 4), function (err) {
-    if (!err) {
-        console.log('.output/final.json -- Actualizado!');
-    } else {
-        console.log('ERROR FATAL!! Al guardar ./final.json');
-        throw err;
-    }
-});
-
-// Actualizar Firebase
+    return arrayEventos;
+};
