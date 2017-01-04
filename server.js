e41a22885b00d350da5c5bdeb045e3e4e0b01c2e
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-01-04 15:40:07 +0000

    Api routes and Firebase functions

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
 var project = require('pillars'),
-    config = require('./config');
+    firebase = require('firebase'),
+    config = require('./config'),
+    harmonizer = require("./harmonizer.js");
+
+// Bring data from datasources
+var data = harmonizer();
+
+// Firebase setup
+firebase.initializeApp({
+  serviceAccount: {
+    projectId: config.firebase.project_id,
+    clientEmail: config.firebase.client_email,
+    privateKey: config.firebase.private_key
+  },
+  databaseURL: "https://"+config.firebase.project_id+".firebaseio.com"
+});
+
+var db = firebase.database();
+var ref = db.ref("events");
 
 // Starting the project
 project.services.get('http').configure({
     port: process.env.PORT || 3000
 }).start();
 
-// Static
+// Define Rutes
+var apiRoute = new Route({
+    id: 'staticRoute',
+    path: '/api',
+    cors: true
+    }, function(gw){
+        gw.redirect("/");
+});
+
+var apiEventsRoute = new Route({
+    id: 'staticRoute',
+    path: 'api/events',
+    cors: true
+    }, function(gw){
+        gw.json(data, {deep:10});
+});
+
 var staticRoute = new Route({
     id: 'staticRoute',
     path: '/*:path',
@@ -17,4 +51,32 @@ var staticRoute = new Route({
 });
 
 // Adding routes objects to the project
+project.routes.add(apiEventsRoute);
+project.routes.add(apiRoute);
 project.routes.add(staticRoute);
+
+
+
+
+function readData() {
+    ref.once("value", function(snapshot) {
+        data = snapshot.val();
+    }, function (errorObject) {
+        console.log("Error:", errorObject);
+    });
+}
+
+function updateData(){
+    ref.set(data, function(error) {
+      if (error) {
+        console.log("Data could not be saved." + error);
+      } else {
+        console.log("Data saved successfully.");
+      }
+    });
+}
+
+//updateData();
+//readData();
+
+// Cron Tasks
