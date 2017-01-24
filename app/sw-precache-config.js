2dd3cb536b496e7a1d3467f5a33ceef54d414beb
Borja Godoy Gago <borja@Borjas-MacBook-Pro.local>
2017-01-24 08:29:48 +0100

    add favicons and splash by the init app

--- app/sw-precache-config.js
+++ app/sw-precache-config.js
@@ -12,8 +12,6 @@
 
 module.exports = {
   staticFileGlobs: [
-    '/index.html',
-    '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
   ],
   navigateFallback: 'index.html',
