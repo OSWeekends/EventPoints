52c230a5117b5bb510f4bf552875852e40438103
Sejas <antonio@sejas.es>
2018-10-27 13:47:49 +0200

    fixed #44, API service done. It reads the api from API_URL environment variable.

---
+++ src/Services/Api/Api.js
@@ -0,0 +1,24 @@
+import { ApiConstants } from '../../Constants';
+class Api {
+  constructor(config) {
+    this.config = config;
+  }
+  /**
+   * Gets a list of events.
+   */
+  async getEvents() {
+    const headers = new Headers();
+    try {
+      const response = await fetch(`${ApiConstants.url}/api/v1/events`, {
+        headers: headers
+      });
+      const events = await response.json();
+      return events;
+    } catch (e) {
+      console.error(`Error while fetching: ${e}`);
+      return [];
+    }
+  }
+}
+
+export const ApiService = new Api();
--- src/components/Events.js
+++ src/components/Events.js
@@ -1,40 +1,23 @@
 import React, { Component } from 'react';
 import Event from './Event';
 import './Events.scss';
+import { ApiService } from '../Services';
 
 class Events extends Component {
-  constructor() {
-    super();
+  state = {
+    loading: true,
+    events: [],
+    current: null
+  };
 
-    this.state = { load: 'loading', events: [] };
-    this.requestEvents = this.requestEvents.bind(this);
-
+  componentWillMount() {
     this.requestEvents();
   }
 
   async requestEvents() {
-    const headers = new Headers();
-    headers.set('Access-Control-Allow-Credentials', 'true');
-    headers.set('Access-Control-Allow-Origin', '*');
-    headers.set('Access-Control-Allow-Headers', '*');
-    headers.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
-
-    const myInit = { headers: headers };
-    try {
-      const eventsResponse = await fetch(
-        'http://localhost:3000/api/v1/events',
-        myInit
-      );
-      const events = await eventsResponse.json();
-      this.setState({ events: events });
-    } catch (e) {
-      console.log(`Error while fetching: ${e}`);
-    }
+    const events = await ApiService.getEvents();
+    this.setState({ events, loading: false });
   }
-
-  state = {
-    current: null
-  };
 
   onSelect = eventID => {
     this.setState({
@@ -43,13 +26,12 @@ class Events extends Component {
   };
 
   render() {
-    const data = this.state.events || [];
-    const { current } = this.state;
+    const { events, current } = this.state;
     const colores = ['red', 'green', 'yellow', '#fabada', 'purple'];
 
     return (
       <ul className="Events">
-        {data.map((event, index) => {
+        {events.map((event, index) => {
           const color = index % 5;
 
           const selected = event.id === current;
