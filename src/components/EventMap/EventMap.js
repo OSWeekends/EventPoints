4ce14715bbdbfa36fd6b07679a77be13a597fb91
VaqueroFontenla <loret.vaquero@gmail.com>
2018-11-06 13:02:45 +0100

    Add markers

--- src/components/EventMap/EventMap.js
+++ src/components/EventMap/EventMap.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import './EventMap.scss';
+import { ApiService } from '../../Services';
 
 class EventMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: 40.4124265,
-      lng: -3.7204109,
-      zoom: 13
+      loading: true,
+      events: [],
+      zoom: 10
     };
   }
 
+  componentWillMount() {
+    this.requestEvents();
+  }
+
+  async requestEvents() {
+    const events = await ApiService.getEvents();
+    this.setState({ events, loading: false });
+  }
+
   render() {
-    const position = [this.state.lat, this.state.lng];
+    const { events } = this.state;
     return (
       <Map
-        className="mapContainer"
+        className="MapContainer"
         style={{ backgroundColor: 'black', height: '500px', widht: '500px' }}
-        center={position}
+        center={[40.3995824, -3.7206383]}
         zoom={this.state.zoom}
       >
         <TileLayer
@@ -26,13 +36,18 @@ class EventMap extends Component {
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            <span>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </span>
-          </Popup>
-        </Marker>
+        {events.map((event, index) => {
+          const position = [event.location.lat, event.location.lng];
+          return (
+            <Marker key={index} position={position}>
+              <Popup>
+                <span>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </span>
+              </Popup>
+            </Marker>
+          );
+        })}
       </Map>
     );
   }
