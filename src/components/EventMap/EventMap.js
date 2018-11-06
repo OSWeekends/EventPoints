3656a3ff5315cdcaebfde673fb8d8857a7996e9d
VaqueroFontenla <loret.vaquero@gmail.com>
2018-11-06 22:40:42 +0100

    Add popup and layers control

--- src/components/EventMap/EventMap.js
+++ src/components/EventMap/EventMap.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, LayersControl } from 'react-leaflet';
+import L from 'leaflet';
 import './EventMap.scss';
 import { ApiService } from '../../Services';
+import euro from '../../images/svg/euro.svg';
+import pointer from '../../images/svg/pointer.svg';
+import calendar from '../../images/svg/calendar.svg';
+
+const { BaseLayer, Overlay } = LayersControl;
 
 class EventMap extends Component {
   constructor(props) {
@@ -24,6 +30,11 @@ class EventMap extends Component {
 
   render() {
     const { events } = this.state;
+    const iconMarker = new L.Icon({
+      iconUrl: require('../../images/marker.png'),
+      iconSize: [50, 58],
+      iconAnchor: [25, 50]
+    });
     return (
       <Map
         className="MapContainer"
@@ -31,23 +42,72 @@ class EventMap extends Component {
         center={[40.3995824, -3.7206383]}
         zoom={this.state.zoom}
       >
-        <TileLayer
-          style={{ backgroundColor: 'black', height: '500px', widht: '500px' }}
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-        {events.map((event, index) => {
-          const position = [event.location.lat, event.location.lng];
-          return (
-            <Marker key={index} position={position}>
-              <Popup>
-                <span>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </span>
-              </Popup>
-            </Marker>
-          );
-        })}
+        <LayersControl position="topright">
+          <BaseLayer checked name="OpenStreetMap.Mapnik">
+            <TileLayer
+              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </BaseLayer>
+
+          <BaseLayer name="OpenStreetMap.BlackAndWhite">
+            <TileLayer
+              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+            />
+          </BaseLayer>
+
+          {events.map((event, index) => {
+            const position = [event.location.lat, event.location.lng];
+            const price = event.price.isFree;
+            const cost = event.price.details;
+            const url = event.source.event_url;
+
+            if (event.price.isFree === true) {
+              this.price = 'Gratis';
+            } else {
+              this.price = cost;
+            }
+
+            return (
+              <Overlay name={event.title}>
+                <Marker position={position} icon={iconMarker}>
+                  <Popup>
+                    <div className="PopUp">
+                      <div className="TitleEvent">
+                        <span>{event.title}</span>
+                      </div>
+                      <div className="InformationEvent">
+                        <div>
+                          <span className="Data">
+                            <img src={calendar} className="icon" />
+                            {event.date}
+                          </span>
+                          <span className="Data">
+                            <img src={pointer} className="icon" />
+                            {event.location.name}
+                          </span>
+                        </div>
+                        <span className="Data">
+                          <img src={euro} className="icon" /> {this.price}
+                        </span>
+                      </div>
+                      <div className="DescriptionEvent">
+                        <span>
+                          Más info:{' '}
+                          <a className="Link" href={event.source.event_url}>
+                            {' '}
+                            Pincha aquí
+                          </a>
+                        </span>
+                      </div>
+                    </div>
+                  </Popup>
+                </Marker>
+              </Overlay>
+            );
+          })}
+        </LayersControl>
       </Map>
     );
   }
