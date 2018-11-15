c1be596c73e0b1ff743f03bb8354f881e6f9e6e0
Sejas <antonio@sejas.es>
2018-11-15 01:27:48 +0100

    Basic event test, find important info and test click on DetailButton

---
+++ src/components/Event/Event.test.js
@@ -0,0 +1,26 @@
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import { eventMock } from './EventMock';
+import Event from './Event';
+
+test('Event has basic info', async () => {
+  const mockOnSelect = jest.fn();
+  const { getByText } = render(
+    <Event event={eventMock} onSelect={mockOnSelect} />
+  );
+  getByText(eventMock.title);
+  getByText(eventMock.abstract);
+});
+
+test('Test click on Detail Button', async () => {
+  const mockOnSelect = jest.fn();
+  const { container } = render(
+    <Event event={eventMock} onSelect={mockOnSelect} />
+  );
+  const DetailButton = container.querySelector('.ButtonDetail');
+  expect(mockOnSelect).toHaveBeenCalledTimes(0);
+  fireEvent.click(DetailButton);
+  expect(mockOnSelect).toHaveBeenCalledTimes(1);
+});
---
+++ src/components/Event/EventMock.js
@@ -0,0 +1,30 @@
+export const eventMock = {
+  abstract:
+    'El email marketing es uno de los canales más potentes de adquisición de clientes. Por ello, TechHub lanza este Expert Talk con Clara Ávila, ...',
+  abstract_details:
+    'El email marketing es uno de los canales más potentes de adquisición de clientes. Por ello, TechHub lanza este Expert Talk con Clara Ávila, responsable de contenido online en Save The Children, con experiencia en The Cocktail y autora del blog claraavilac. En él se profundizará en las distintas estrategias de marketing de contenidos y cómo obtener los mejores resultados.\n\nAgenda:- Email marketing - presente y futuro- Inbound marketing y categorías de contenido- Planificación de contenidos y automatización- Cómo preparar una campaña: contenidos del mensaje, prototipado- Cómo conseguir nuevos suscriptores- Analítica y KPIs- Best Practices',
+  date: '2017-01-24T19:00:00Z',
+  location: {
+    lat: 40.41249699999999,
+    lng: -3.7182264000000487,
+    name: 'Campus Madrid',
+    notes: 'Auditorium',
+  },
+  price: {
+    details: 'estimado',
+    isFree: true,
+    isTrusted: false,
+  },
+  source: {
+    event_url:
+      'https://www.campus.co/madrid/en/events/ag1zfmd3ZWItY2FtcHVzcj8LEgZDYW1wdXMiBFJvb3QMCxIGQ2FtcHVzIgZtYWRyaWQMCxIFRXZlbnQiEmExOWowMDAwMDAxVHFhZUFBQww',
+    logo:
+      'http://tetuanvalley.com/wp-content/uploads/2016/03/opengraph-768x403.jpg',
+    name: 'Campus Madrid',
+    url: 'http://campus.co/madrid',
+  },
+  target_url:
+    'https://www.techhub.com/events/improve-your-email-marketing-strategy-with-clara',
+  title: 'Mejora tu estrategia de Email Marketing con Clara Ávila',
+  id: 'b63ba480-f937-4ac2-8f16-46ce738a5231',
+};
