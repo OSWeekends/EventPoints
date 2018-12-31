5d3e7b80ea4939b607ec7a6e25e251c7fb0ff8f7
VaqueroFontenla <loret.vaquero@gmail.com>
2018-12-31 13:26:31 +0100

    Improve map

--- src/components/EventMap/EventMap.js
+++ src/components/EventMap/EventMap.js
@@ -7,8 +7,6 @@ import euro from '../../images/svg/euro.svg';
 import pointer from '../../images/svg/pointer.svg';
 import calendar from '../../images/svg/calendar.svg';
 
-const { BaseLayer, Overlay } = LayersControl;
-
 class EventMap extends Component {
   constructor(props) {
     super(props);
@@ -43,83 +41,75 @@ class EventMap extends Component {
         zoom={this.state.zoom}
       >
         <LayersControl position="topright">
-          <BaseLayer checked name="OpenStreetMap.Mapnik">
+          <LayersControl.BaseLayer checked name="OpenStreetMap.Mapnik">
             <TileLayer
               attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-          </BaseLayer>
+          </LayersControl.BaseLayer>
 
-          <BaseLayer name="OpenStreetMap.BlackAndWhite">
+          <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
             <TileLayer
               attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
             />
-          </BaseLayer>
-
-          {events.map((event, index) => {
-            let position;
-            try {
-              if (!isNaN(event.location.lat) && !isNaN(event.location.lng)) {
-                position = [event.location.lat, event.location.lng];
-              }
-            } catch (e) {
-              position = [0, 0];
-            }
-            // const price = event.price.is_free;
-            const cost = event.price.details;
-            // const url = event.source.event_url;
-
-            if (event.price.is_free === true) {
-              this.price = 'Gratis';
-            } else {
-              this.price = cost;
-            }
-
-            return (
-              <Overlay name={event.title}>
-                <Marker position={position} icon={iconMarker}>
-                  <Popup>
-                    <div className="PopUp">
-                      <div className="TitleEvent">
-                        <span>{event.title}</span>
-                      </div>
-                      <div className="InformationEvent">
-                        <div>
-                          <span className="Data">
-                            <img
-                              src={calendar}
-                              className="icon"
-                              alt="calendar"
-                            />
-                            {event.date}
-                          </span>
-                          <span className="Data">
-                            <img src={pointer} className="icon" alt="pointer" />
-                            {event.location.name}
-                          </span>
-                        </div>
-                        <span className="Data">
-                          <img src={euro} className="icon" alt="price" />{' '}
-                          {this.price}
-                        </span>
-                      </div>
-                      <div className="DescriptionEvent">
-                        <span>
-                          Más info:{' '}
-                          <a className="Link" href={event.target_url}>
-                            {' '}
-                            Pincha aquí
-                          </a>
-                        </span>
-                      </div>
-                    </div>
-                  </Popup>
-                </Marker>
-              </Overlay>
-            );
-          })}
+          </LayersControl.BaseLayer>
         </LayersControl>
+
+        {events.map((event, index) => {
+          let position;
+          if (!isNaN(event.location.lat) && !isNaN(event.location.lng)) {
+            position = [event.location.lat, event.location.lng];
+          } else {
+            position = [0, 0];
+          }
+          // const price = event.price.is_free;
+          const cost = event.price.details;
+          // const url = event.source.event_url;
+
+          if (event.price.is_free === true) {
+            this.price = 'Gratis';
+          } else {
+            this.price = cost;
+          }
+
+          return (
+            <Marker key={index} position={position} icon={iconMarker}>
+              <Popup>
+                <div className="PopUp">
+                  <div className="TitleEvent">
+                    <span>{event.title}</span>
+                  </div>
+                  <div className="InformationEvent">
+                    <div>
+                      <span className="Data">
+                        <img src={calendar} className="icon" alt="calendar" />
+                        {event.date}
+                      </span>
+                      <span className="Data">
+                        <img src={pointer} className="icon" alt="pointer" />
+                        {event.location.name}
+                      </span>
+                    </div>
+                    <span className="Data">
+                      <img src={euro} className="icon" alt="price" />{' '}
+                      {this.price}
+                    </span>
+                  </div>
+                  <div className="DescriptionEvent">
+                    <span>
+                      Más info:{' '}
+                      <a className="Link" href={event.target_url}>
+                        {' '}
+                        Pincha aquí
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </Popup>
+            </Marker>
+          );
+        })}
       </Map>
     );
   }
