3926ac24e091805e0b3c52815c70d083c4c0c947
VaqueroFontenla <loret.vaquero@gmail.com>
2019-01-08 22:25:24 +0100

    Work in progress

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import './Header.scss';
+import IconMarker from '../Shared/Svg/Icon-marker';
 
 class Header extends Component {
   render() {
-    return <div className="Header">HEADER</div>;
+    return (
+      <div className="Header">
+        <span className="IconMarker">
+          <IconMarker />
+        </span>
+        <h1 className="title">
+          Events
+          <b>Points</b>
+        </h1>
+      </div>
+    );
   }
 }
 
