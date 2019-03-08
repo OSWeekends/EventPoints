09954f0c119383823698657ca9367759f445cf66
Loreto Vaquero Fontenla <36046100+VaqueroFontenla@users.noreply.github.com>
2019-03-08 09:51:07 +0100

    Add some styles

---
+++ Search.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import IconCalendar from '../Shared/Svg/Icon-calendar';
+import IconLens from '../Shared/Svg/Icon-lens';
+import IconEuro from '../Shared/Svg/Icon-euro';
+import Calendar from 'react-calendar';
+import './Search.scss';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCalendar: false,
+    };
+
+    this.showCalendar = this.showCalendar.bind(this);
+  }
+
+  // showCalendar(e) {
+  //   this.state.showCalendar === false
+  //     ? this.setState({
+  //         showCalendar: true,
+  //       })
+  //     : this.setState({
+  //         showCalendar: false,
+  //       });
+  // }
+
+  render() {
+    const {
+      filterEventByTitle,
+      filterEventByDay,
+      filterEventByMoney,
+    } = this.props;
+    console.log(filterEventByMoney);
+
+    return (
+      <div className="Search">
+        <IconEuro filterEventByMoney={filterEventByMoney} />
+        <hr />
+        <IconCalendar onClick={this.showCalendar} />
+        {this.state.showCalendar ? (
+          <Calendar onChange={filterEventByDay} />
+        ) : null}
+        <div className="InputSearch">
+          <label htmlFor="inputEvents" />
+          <input
+            className="Input"
+            type="text"
+            name="inputEvents"
+            placeholder="Buscador de eventos"
+            value={this.props.name}
+            onChange={filterEventByTitle}
+          />
+          <IconLens className="Lens" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
